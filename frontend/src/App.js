a8946ec03b01f6e0ae60700bc69a291763fe5b0c
John <gamingvyse@gmail.com>
2021-02-28 23:02:17 -0600

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import Index from './components/register/index'
+
+function App() {
+  return (
+    <div className="d-flex pt-4 justify-content-center">
+      <Index/>
+    </div>
+  );
+}
+
+export default App;
