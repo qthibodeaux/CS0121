1aa601b82e31b4530cb917c75cb8c57be809601b
John <gamingvyse@gmail.com>
2021-03-07 17:19:16 -0600

    Update index.js

--- frontend/src/components/login/index.js
+++ frontend/src/components/login/index.js
@@ -3,17 +3,21 @@ import { useForm } from 'react-hook-form'
 import axios from 'axios'
 
 function LoginForm () {
-  const [emailError, setEmailError] = useState(true)
-  const [passwordError, setPasswordError] = useState(true)
-  const { register, handleSubmit } = useForm()
+  const [emailError, setEmailError] = useState(false)
+  const [passwordError, setPasswordError] = useState(false)
+  const { register, handleSubmit, errors } = useForm()
   const onSubmit = async (data) => {
     axios.post("/login", {
       email: data.email,
       password: data.password,
     })
     .then(function (response){
-      if (response.data.emailError) setEmailError(false)
-      if (response.data.passwordError) setPasswordError(false)
+      console.log(response)
+      if (response.data.emailError) {
+        setEmailError(true)
+        console.log(emailError)
+      }
+      if (response.data.passwordError) setPasswordError(true)
       if (response.data) {
         localStorage.setItem("token", response.data.token)
       }
@@ -30,13 +34,15 @@ function LoginForm () {
           <h2><strong>Welcome Back!</strong></h2>
           <div className="mb-3">
             <label className="form-label">Email address</label>
-            <input type="text" className="form-control" placeholder="Enter email" name="email" ref={register({ required: true })} />
-            {emailError ? null : "The email doesn't exist"}
+            <input type="text" className={ errors.email ? "form-control is-invalid" : "form-control"} placeholder="Enter email" name="email" ref={register({ required: "Email is required" })} />
+            { emailError ? (<div className="invalid-feedback">
+              Please select a valid state.
+            </div>) : " huh "}
           </div>
           <div className="mb-3">
             <label className="form-label is-valid">Password</label>
-            <input type="password" className="form-control" placeholder="Password" name="password" ref={register({ required: true })} />
-            {passwordError ? null : "Your password is incorrect"}
+            <input type="password" className="form-control" placeholder="Password" name="password" ref={register({ required: "Passord is required" })} />
+            { passwordError && <div className="invalid-feedback">Your password is incorrect</div>}
           </div>
           <button className="btn btn-primary text-light" type="submit">Login</button>
           <div className="d-flex justify-content-center">
